64d6c0a6d877500c4faef4a1f5668c484990c363
AndreeOak <andreesoul@gmail.com>
2025-09-21 20:58:45 -0300

    Creating Post Request for new users

--- setup/users/views.py
+++ setup/users/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    context ={
+        "form" : UserCreationForm()
+    }
+    return render(request, 'users/register.html', context)
