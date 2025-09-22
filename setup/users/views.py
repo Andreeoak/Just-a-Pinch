4dd39a6c744329b49323e8c4f0d333ae478485a3
AndreeOak <andreesoul@gmail.com>
2025-09-22 09:54:37 -0300

    Feedback on registering user

--- setup/users/views.py
+++ setup/users/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def register(request):
-    if request.method =="POST":
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, your account has been successfully created!')
             return redirect("myapp:index")
-        
-    context ={
-        "form" : UserCreationForm()
+    else:
+        form = UserCreationForm() 
+
+    context = {
+        "form": form
     }
-    return render(request, 'users/register.html', context)
+    return render(request, "users/register.html", context)
