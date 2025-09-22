6de7dd9d0b9a1625fbedb340f5818f2b1fef4721
AndreeOak <andreesoul@gmail.com>
2025-09-22 10:34:19 -0300

    Login view and redirect to main page

--- setup/users/urls.py
+++ setup/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 #namespacing
@@ -6,4 +7,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name= 'users/login.html'), name="login" ),
 ]
